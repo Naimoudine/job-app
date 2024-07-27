7d8678a756e617e68cfacab9db4b4dcfbc9115da
Naïm <naimoudinesaidi@gmail.com>
2024-07-27 23:03:27 +0200

    added the rest of eslint config

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,21 +1,28 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+// eslint.config.js
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  // Type of the project. 'lib' for libraries, the default is 'app'
+  type: "app",
+
+  // Enable stylistic formatting rules
+  // stylistic: true,
+
+  react: true,
+
+  // Or customize the stylistic rules
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: "single", // or 'double'
   },
-}
+
+  // Disable jsonc and yaml support
+  jsonc: false,
+  yaml: false,
+
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  ignores: [
+    "**/fixtures",
+    // ...globs
+  ],
+});
