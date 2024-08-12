e8b23e9b2eb299e9905c448eb3c5cd7f75e12d8c
Naïm <naimoudinesaidi@gmail.com>
2024-08-12 17:53:07 +0200

    wip company page

--- client/src/components/CompanyCard.jsx
+++ client/src/components/CompanyCard.jsx
@@ -1,10 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 export default function CompanyCard({ company }) {
-  console.log(company);
+  const navigate = useNavigate();
+
   return (
-    <article className="w-full md:w-[25rem] h-[18rem] bg-gray-200 p-4 rounded-lg flex flex-col gap-2">
+    <article
+      className="w-full md:w-[25rem] bg-gray-200 p-4 rounded-lg flex flex-col gap-2"
+      onClick={() => navigate(`/companies/${company.id}`)}
+    >
       <h3>{company.name}</h3>
       <p>{company.description}</p>
-      <p>{company.location}</p>
+      <p>
+        <span className="font-semibold">Capital : </span>
+        {company.capital}
+      </p>
+      <p>
+        <span className="font-semibold">Date of create :</span>{" "}
+        {new Date(company.creation_date).toDateString()}
+      </p>
+      <p>
+        <span className="font-semibold">Location : </span>
+        {company.location}
+      </p>
     </article>
   );
 }
